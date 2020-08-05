e629f6355ce42351ad3e208f5acebeb51ad006f8
Rogerio Hanssen <ahanssen@gmail.com>
2020-08-05 01:02:55 -0300

    Rotas, página 404, cadastro de vídeos e categorias

---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageTemplate from '../../../components/PageTemplate';
+import { Link } from 'react-router-dom';
+
+function CadastroVideo() {
+    return (
+      <PageTemplate>
+        <h1>Cadastro de Vídeo</h1>
+
+        <Link to="/cadastro/Categoria">
+            Cadastrar Categoria
+        </Link>
+      </PageTemplate>
+    )
+  }
+
+  export default CadastroVideo;
